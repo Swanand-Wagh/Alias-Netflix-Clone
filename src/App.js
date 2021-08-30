08aa94555f062ddc15241741d5587a3b6f926448
Swanand-Wagh <swanandwagh7@gmail.com>
2021-08-31 00:18:03 +0530

    basic api & requests setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import './App.css';
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      Hello Awesome
-    </div>
+    <>
+      <div className="App">
+        
+      </div>
+    </>
   );
-}
+};
 
 export default App;
