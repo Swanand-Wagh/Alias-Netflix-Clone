f4fbf35d0f9701392870b3d5285043df52d4fce1
Swanand-Wagh <swanandwagh7@gmail.com>
2021-08-31 12:12:29 +0530

    Navbar udated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import NotificationsIcon from "@material-ui/icons/Notifications";
 import "./Nav.css";
 
 const Nav = () => {
@@ -17,16 +19,30 @@ const Nav = () => {
   return (
     <>
       <div className={`nav ${show && "nav_black"}`}>
-        <img
-          src={process.env.PUBLIC_URL + "/logo.png"}
-          alt="Netflix Logo"
-          className="nav_logo"
-        />
-        <img
-          className="nav_avatar"
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHg_t3WBJBy4SEC_9uU-gi71PNXXdInw5uRQ&usqp=CAU"
-          alt=""
-        />
+        <div>
+          <img
+            src={process.env.PUBLIC_URL + "/logo.png"}
+            alt="Netflix Logo"
+            className="nav_logo"
+          />
+          <div className="nav_links">
+            <p>Home</p>
+            <p>TV Shows</p>
+            <p>Movies</p>
+            <p>New & Popular</p>
+            <p>My List</p>
+          </div>
+        </div>
+        <div className="nav_links profile">
+          <SearchIcon style={{ marginRight: "1.25rem" }} />
+          <p>CHILDREN</p>
+          <NotificationsIcon />
+          <img
+            className="nav_avatar"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHg_t3WBJBy4SEC_9uU-gi71PNXXdInw5uRQ&usqp=CAU"
+            alt=""
+          />
+        </div>
       </div>
     </>
   );
